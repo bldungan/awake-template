1a54bcb72ca9af22f875142318067efd0f143afe
Chris Levine <chrislevine24@gmail.com>
2020-04-04 16:17:10 -0400

    logo?

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -12,7 +12,7 @@ export default {
   siteName: 'The Ben Dugan (dot com)',
   tagline: "It's true... I'm the only one!",
   featureImage: '/uploads/home-hero.jpg',
-  logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
+  logo: '/logo.svg', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
     id: process.env.GOOGLE_ANALYTICS_ID
